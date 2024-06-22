c8c3c8c4c3cc7ebee96b6da53c50a3d4d00ec970
Zhakshylyk <zhakshylyknasipbekov@gmail.com>
2024-06-22 16:23:00 +0300

    expense-tracker create folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { NewExpense } from "./components/new-expense/NewExpense";
+
+
+
+const App = () => {
+  return (
+    <div>
+      <NewExpense />
+    </div>
+  );
+};
+export default App;
