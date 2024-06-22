55215c3ad835b15ae6818a0bf2621064de915083
Zhakshylyk <zhakshylyknasipbekov@gmail.com>
2024-06-22 16:48:05 +0300

    add EXPENSES array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NewExpense } from "./components/new-expense/NewExpense";
+import { EXPENSES } from "./utils/constants";
 
 const App = () => {
+  const [expenses, setExpenses] = useState(EXPENSES);
+
   return (
     <div>
       <NewExpense />
